2cd8edefd327cc9ef721aa3f21d17978303ee538
mmsalim <happymarja@gmail.com>
2025-03-08 21:13:45 +0530

    initial commit

---
+++ projects/web-components/src/lib/web-components.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-web-components',
+  imports: [],
+  template: `
+    <p>
+      web-components works!
+    </p>
+  `,
+  styles: ``
+})
+export class WebComponentsComponent {
+
+}
---
+++ projects/web-components/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of web-components
+ */
+
+export * from './lib/web-components.service';
+export * from './lib/web-components.component';
