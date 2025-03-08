0c9681c07320ecb1606f0f9e306c14d53675db58
mmsalim <happymarja@gmail.com>
2025-03-08 21:28:36 +0530

    standalone property added

--- projects/web-components/src/lib/web-components.component.ts
+++ projects/web-components/src/lib/web-components.component.ts
@@ -2,14 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'lib-web-components',
+  standalone: true,
   imports: [],
-  template: `
-    <p>
-      web-components works!
-    </p>
-  `,
-  styles: ``
+  template: ` <p>web-components works!</p> `,
+  styles: ``,
 })
-export class WebComponentsComponent {
-
-}
+export class WebComponentsComponent {}
